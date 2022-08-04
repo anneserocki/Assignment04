1d2da6323fa21ef89ed10222b4ac2db774a8918a
Your Name <anneserocki@gmail.comom>
2022-08-04 16:27:12 -0700

    finish1

---
+++ js/01-functions-practice.js
@@ -0,0 +1,30 @@
+//STEP 1
+// Method 1
+// function division() {
+//     let x = parseFloat(prompt(`Please enter a number`));
+//     let y = x/2;    
+//     console.log(`Half of ${x} is ${y}`);    
+// }
+// division();
+
+// Method 2
+// let x = parseFloat(prompt(`Please enter a number`));
+// const division = function(z) {
+//     let y=x/2;
+//     console.log(`Half of ${x} is ${y}`)
+// };
+// division(x);
+
+// Method 3
+let x = parseFloat(prompt(`Please enter a number`));
+const division = (z) => {console.log(`Half of ${x} is ${x/2}`) };
+division(x)
+
+//STEP 2
+
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
