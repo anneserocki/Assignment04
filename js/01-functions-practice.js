85ee90803a71a7bc6f3fb29a633ac68c19beb44f
Your Name <anneserocki@gmail.comom>
2022-08-04 22:17:40 -0700

    finished 3

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,29 +1,56 @@
 //STEP 1
 // Method 1
-// function division() {
+// function halfNumber() {
 //     let x = parseFloat(prompt(`Please enter a number`));
 //     let y = x/2;    
 //     console.log(`Half of ${x} is ${y}`);    
 // }
-// division();
+// halfNumber();
 
 // Method 2
 // let x = parseFloat(prompt(`Please enter a number`));
-// const division = function(z) {
+// const halfNumber = function(z) {
 //     let y=x/2;
 //     console.log(`Half of ${x} is ${y}`)
 // };
-// division(x);
+// halfNumber(x);
 
 // Method 3
-let x = parseFloat(prompt(`Please enter a number`));
-const division = (z) => {console.log(`Half of ${x} is ${x/2}`) };
-division(x);
+// let x = parseFloat(prompt(`Please enter a number`));
+// const halfNumber = (z) => {console.log(`Half of ${x} is ${x/2}`) };
+// halfNumber(x);
 
 //STEP 2
+// Method1
+// let x = parseFloat(prompt(`Please enter a number`));
+// const squareNumber = (z) => {console.log(`The result of squaring the number ${x} is ${x * x}.`)};
+// squareNumber(x);
 
+// Method2
+// let x = parseFloat(prompt(`Please enter a number`));
+// function squareNumber(z) {    
+//     console.log(`The result of squaring the number ${x} is ${x * x}.`);    
+// };
+// squareNumber(x);
 
 //STEP 3
+// Method1
+// let firstNum = parseFloat(prompt(`Please enter the first number`));
+// let secondNum = parseFloat(prompt(`Please enter the second number`));
+// function percentOf(x,y) {
+//     let percentNum = ((x / y)*100);
+//     console.log(`${x} is ${percentNum}% of ${y}.`);
+// };
+// percentOf(firstNum, secondNum);
+
+// Method2
+let firstNum = parseFloat(prompt(`Please enter the first number`));
+let secondNum = parseFloat(prompt(`Please enter the second number`));
+let percentNum = ((firstNum / secondNum)*100);
+const percentOf = (x, y) => {
+    console.log(`${x} is ${percentNum}% of ${y}.`);
+}
+percentOf(firstNum, secondNum);
 
 //STEP 4
 
