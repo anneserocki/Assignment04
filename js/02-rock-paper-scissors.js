1a30720dd5965daf3a87c01bc1d08cc2efdf5da6
Your Name <anneserocki@gmail.comom>
2022-08-17 16:24:42 -0700

    commit02

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,49 +1,60 @@
-// Define variables of computer
-let computerRock, computerScissor, computerPepper;
-// Define mine variable
+// Define my variable
 let myChoice = prompt(`Please enter your answer: rock, or scissor, or pepper`);
 switch (myChoice) {
-    case `rock`: alert(`My answer is rock`);
-    myChoice === 'rock'
-    break;
-    case `scissor`: alert(`My answer is scissor`);
-    myChoice === 'scissor'
-    break;
-    case `pepper`: alert(`My answer is pepper`);
-    myChoice === 'pepper';
+    case `rock`:
+        console.log(`My answer is rock`);
+        myChoice === 'rock';
+        break;
+    case `scissor`:
+        console.log(`My answer is scissor`);
+        myChoice === 'scissor'
+        break;
+    case `pepper`:
+        console.log(`My answer is pepper`);
+        myChoice === 'pepper';
+        break;
     default:
-        alert(`Your answer is wrong, please re-enter your answer among rock, scissor, or pepper`)
-};
+        alert(`Your answer is wrong, please re-enter your answer among rock, scissor, or pepper`);
+        break;
+    };
 
-// Get computer's answers
-let computerChoice = Math.floor(Math.random()*10+2);
-console.log(computerChoice);
-if (computerChoice >-1 && computerChoice <4) {
-    computerRock = alert(`rock`);
-    computerRock === 'rock'; 
-} else if (computerChoice > 3 && computerChoice < 8) {
-    computerPepper = alert(`pepper`);
-    computerRock === 'pepper';
-} else {
-    computerScissor = alert(`scissor`);
-    computerRock === 'scissor';
+// Define variables of computer and get computer's answers
+let computerNumber = Math.floor(Math.random()*10+2);
+let computerChoice;
+console.log(computerNumber);
+if (computerNumber >-1 && computerNumber <4) {
+    console.log(`Your answer is rock`);
+    computerChoice === 'rock'; 
+} else if (computerNumber > 3 && computerNumber < 8) {
+    console.log(`Your answer is pepper`);
+    computerChoice === 'pepper';
+} else if (computerNumber > 7 && computerNumber < 12) {
+    console.log(`Your answer is scissor`);
+    computerChoice === 'scissor';
 }; // End if...else if for computer's answer
 
 // Define a function for playing game
+let player1 = 'rock' || 'pepper' || 'scissor'
+let player2 = 'rock' || 'pepper' || 'scissor'
+function playGame(player1, player2){
 
-function playGame(){
-    let player1, player2;
-    if ('rock' && 'scissor') {
-        player1 === 'rock';
-        console.log(player1 + 'is win!');
-    } else if ('scissor'  && 'pepper'){
-        player1 === 'scissor';
-        console.log(player1 + 'is win!');  
-    } else if ('pepper'  && 'rock'){
-        player1 === 'pepper';
-        console.log(player1 + 'is win!');
-    } else if (('pepper'  && 'pepper') || ('rock'  && 'rock') ||('scissor'  && 'scissor')){
+    if (player1 ==='rock' && player2 === 'rock' || player1 === 'pepper' && player2 === 'pepper' ||player1 === 'scissor' && player2 ==='scissor') {
         console.log('The result ends in a tie, please re-play!');
-    } ;
+    } else if (player1 ==='rock' && player2 === 'scissor') {
+        console.log('I win!');
+    } else if (player1 ==='scissor' && player2 === 'pepper') {
+        console.log('I win!');  
+    } else if (player1 ==='pepper' && player2 === 'rock') {        
+        console.log('I win!');
+    } else if (player1 ==='scissor' && player2 === 'rock'){
+        console.log('You win!');        
+    } else if (player1 ==='pepper' && player2 === 'scissor'){
+        console.log('You win!'); 
+    } else if (player1 ==='rock' && player2 === 'pepper') {
+        console.log('You win!'); 
+    } else {
+        console.log('Something is wrong, please re-start playing.')
+    };
 };
-playGame(computerChoice, myChoice);
+
+playGame(myChoice, computerChoice);
